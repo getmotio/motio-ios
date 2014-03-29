c5b5e19e947837bfb643b55d407831bc712a2dd7
Paul Ballesty <paulballesty@gmail.com>
2014-03-29 16:43:33 -0300

    Empty project

---
+++ Motio/MotioTests/MotioTests.m
@@ -0,0 +1,34 @@
+//
+//  MotioTests.m
+//  MotioTests
+//
+//  Created by Paul Ballesty on 3/29/14.
+//  Copyright (c) 2014 Motio. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface MotioTests : XCTestCase
+
+@end
+
+@implementation MotioTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
